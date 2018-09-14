13fcf9b92c63ac465226aa83f6c9118f0f48b880
Michael Betz <mbetz@lbl.gov>
2018-09-13 19:59:39 -0700

    Sounds good now

--- audio_effects/ccd_tone_app.py
+++ audio_effects/ccd_tone_app.py
@@ -5,24 +5,22 @@
 
 import argparse
 from numpy import *
-import scipy.signal
 from matplotlib.pyplot import *
 import pyaudio
-
-from time import sleep
 import threading
 from serial import Serial
 import atexit
 
+
 def main():
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument("--tty_dump", default="/dev/ttyUSB0",
-        help="UartMemoryDumper for data")
-    parser.add_argument("--br_dump", default=115200, type=int,
-        help="UartMemoryDumper baudrate")
+    parser.add_argument("--tty_dump", default="/dev/ttyUSB0", help="UartMemoryDumper data")
+    parser.add_argument("--br_dump", default=115200, type=int, help="UartMemoryDumper baudrate")
+    parser.add_argument("--freq_file", help="File with a list of frequencies to map to the CCD pixels. Falls back on a pentatonic scale")
+    parser.add_argument("--base_freq", default=25, type=float, help="Pentatonic scale: fundamental frequency in Hz")
+    parser.add_argument("--n_octaves", default=7, type=int, help="Pentatonic scale: number of octaves")
     args = parser.parse_args()
 
-    ccdData = zeros(128, dtype=float)
     #----------------------------------
     # Setup pyaudio, write audio stream
     #----------------------------------
@@ -36,39 +34,59 @@ def main():
         output=True
     )
 
+    #----------------------------------
+    # Load freq_file
+    #----------------------------------
+    if args.freq_file:
+        # Load frequency list from a file
+        freq = genfromtxt(args.freq_file, skip_header=1, usecols=(1), comments=None)
+        print(args.freq_file, freq)
+    else:
+        # Construct a pentatonic scale
+        # https://www.lightnote.co/music-theory/pentatonic/
+        ratios = array([1, 9/8, 5/4, 3/2, 5/3])
+        scale = []
+        for i in range(args.n_octaves):
+            scale.extend(args.base_freq * ratios * 2**i)
+        freq = array(scale)
+    omega = freq * 2 * pi / F_SAMPLE    # Tone normalized frequency
+    amps = zeros_like(freq)             # Tone amplitude
+    phs = zeros_like(freq)              # Tone initial phase
+    # phs = 2 * pi * (random.rand(freq.size) * 2 - 1)
+
     def audioPlayer():
-        freqs = logspace(-2, 4.5, 128) * 2 * pi / F_SAMPLE
-        phs = 2 * pi * (random.rand(freqs.size) * 2 - 1)
-        amps = ones_like(freqs)
-        chunk = arange(128)  # How many samples to generate per iteration
-        offs = 1
-        freqsv, chunkv = meshgrid(freqs, chunk)
+        chunk = arange(32)  # How many samples to generate per iteration
+        iSample = 1         # Sample index
+        omega_v, chunk_v = meshgrid(omega, chunk)
         while True:
-            # phs = 2 * pi * ccdData
-            amps[:] = ccdData
-            dats = amps * sin(freqsv * (offs + chunkv) + phs)
+            dats = amps * sin(omega_v * (iSample + chunk_v) + phs)
             samples = mean(dats, 1)
             stream.write((samples * 2**15).astype(int16).tostring())
-            offs += chunk.size
+            iSample += chunk.size
     threading.Thread(target=audioPlayer).start()
 
     #----------------------------------------------
-    # Setup serial reader thread
+    # Setup plot and serial reader thread
     #----------------------------------------------
     # Line plot
     fig, ax = subplots(1, 1, figsize=(6, 3))
-    l, = ax.plot(arange(128), zeros(128), "-o")
-    ax.axis((0, 128, 0, 1))
+    l, = ax.plot(freq / 1e3, amps, "-o")
+    ax.axis((amin(freq / 1e3), amax(freq / 1e3), 0, 1))
+    ax.set_xlabel("Frequency [kHz]")
+    # ax.set_xscale("log")
     fig.tight_layout()
     ser = Serial(args.tty_dump, args.br_dump)
 
     def read_from_port():
+        ccdPos = linspace(freq[0], freq[-1], 128)
         while ser.isOpen():
             ser.read_until(b"\x42")
             buf = ser.read(256)
-            ccdData[:] = (fromstring(buf, dtype=">u2").astype(float) / 4096)**2
+            ccdData = fromstring(buf, dtype=">u2").astype(float) / 4096
+            # linearly map the CCD pixel 0 ... 128 to freq[0] ... freq[-1]
+            amps[:] = interp(freq, ccdPos, ccdData**2)
             # Update plot
-            l.set_ydata(ccdData)
+            l.set_ydata(amps)
             fig.canvas.draw_idle()
     atexit.register(ser.close)
     threading.Thread(target=read_from_port).start()
